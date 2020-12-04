287841f62f89548bde1e4924564449620483594c
leonardo.daniel <161800@utags.edu.mx>
2020-12-04 03:09:39 -0600

    Login con password encriptado

--- server/routes/categoria.js
+++ server/routes/categoria.js
@@ -2,6 +2,7 @@ const express = require('express');
 const _ = require('underscore');
 const app = express();
 const Categoria = require('../models/categoria');
+const { response } = require('./usuario');
 
 app.get('/categoria', (req, res) => {
     let desde = req.query.desde || 0;
@@ -51,5 +52,46 @@ app.post('/categoria', (req, res) => {
         });
     });
 });
+app.put('/categoria/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['descripcion', 'usuario']);
+
+    Categoria.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, catDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ocurrio un error al momento de actualizar',
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            msg: 'La categoria fue actualizada con exito',
+            catDB
+        });
+    });
+});
+
+app.delete('/categoria/:id',(req, res)=>{
+    let id = req.params.id;
+
+    Categoria.findByIdAndRemove(id, {context: 'query'}, (err, catDB) =>{
+        if(err){
+            return res.status(400).json({
+                ok:false,
+                msg:'Ocurrio un error al momento de eliminar',
+                err
+            })
+        }
+
+        res.json({
+            ok: true,
+            msg: 'La categoria fue eliminada con exito',
+            catDB
+        })
+    })
+})
+
 
 module.exports = app;
