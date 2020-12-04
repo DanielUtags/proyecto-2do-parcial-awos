4cb2478fdec8bc474ab984a77c2636bb417b66e6
leonardo.daniel <161800@utags.edu.mx>
2020-12-03 20:17:17 -0600

    Realizada la ruta de ususarios con la funcion POST con BD

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,30 +1,45 @@
 const express = require('express');
+const Usuario = require('../models/usuario') 
 const app = express();
 
   app.get('/usuario', function (req, res) {
-    res.json({
-      ok: 200,
-      mensaje: 'Usuarios consultados con exito'
+    Usuario.find({}).exec((err,usuarios)=>{
+      if(err){
+        return res.status(400).json({
+          ok:false,
+          msg:'Ocurrioun error al momento de consultar',
+          err
+        })
+      }
+      res.json({
+        ok:true,
+        msg: 'Lista de usuarios obtenida con exito',
+        conteo: usuarios.length,
+        usuarios
+      })
     });
   })
   
   app.post('/usuario', function(req,res){
-    let nombre = req.body.nombre;
     let body = req.body;
-  
-    if(nombre == undefined){
-      res.status(400).json({
-        ok: 400,
-        mensaje:'Favor de enviar el valor del nombre'
-      });
-    }else{
-  
-     res.json({
-       ok: 200,
-        mensaje: 'Usuario insertado con exito',
-        body: body
-      });
-    }
+    let usr = new Usuario ({
+      nombre:body.nombre,
+      email: body.email,
+      password: body.password
+    });
+    usr.save((err,usrDB)=>{
+      if (err){
+       return res.status(400).json({
+         ok:false,
+         err
+       })
+      }
+      res.json({
+        ok: true,
+        msg: 'Usuario insertado con exito',
+        usrDB
+      })
+    })
   });
   
   app.put('/usuario/:id/:nombre', function(req,res){
